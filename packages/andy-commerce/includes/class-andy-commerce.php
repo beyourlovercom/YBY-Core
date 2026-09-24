ab23c6fd075a9e686408c5a121c2d7236b198fb0
beyourlovercom <203355932+beyourlovercom@users.noreply.github.com>
2026-09-24 16:52:47 +0800

    ACV1-4 boot shipping promotion runtime from Commerce

--- packages/andy-commerce/includes/class-andy-commerce.php
+++ packages/andy-commerce/includes/class-andy-commerce.php
@@ -11,6 +11,8 @@ require_once ANDY_COMMERCE_PLUGIN_DIR . 'admin/class-andy-commerce-admin.php';
 
 class Andy_Commerce {
 	public function run() {
+		Andy_Commerce_Shipping_Promotion_Policy::boot();
+		Andy_Commerce_Shipping_Promotion_Runtime::boot();
 		if ( is_admin() ) {
 			$admin = new Andy_Commerce_Admin();
 			add_action( 'admin_menu', array( $admin, 'add_admin_menu' ), 35 );
