d45b2c66df3cff815156c3431995e8ecc41f267b
beyourlovercom <203355932+beyourlovercom@users.noreply.github.com>
2026-09-24 16:09:51 +0800

    ACV1-2 add Commerce runtime skeleton

---
+++ packages/andy-commerce/includes/class-andy-commerce.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Andy Commerce runtime bootstrap.
+ *
+ * @package Andy_Commerce
+ */
+
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+require_once ANDY_COMMERCE_PLUGIN_DIR . 'admin/class-andy-commerce-admin.php';
+
+class Andy_Commerce {
+	public function run() {
+		if ( is_admin() ) {
+			$admin = new Andy_Commerce_Admin();
+			add_action( 'admin_menu', array( $admin, 'add_admin_menu' ), 35 );
+		}
+	}
+}
